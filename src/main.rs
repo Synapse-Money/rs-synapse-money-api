b7d3531fdfc69a5f201a0008e1c06dd3d9c48af4
Danrley Senegalha Pires <senegalha@DESKTOP-4OOSTH1.localdomain>
2025-11-18 14:34:09 -0300

    chore: initial Rust project setup with Axum

---
+++ src/main.rs
@@ -0,0 +1,31 @@
+use axum::{routing::get, Router};
+use std::net::SocketAddr;
+use tower_http::trace::TraceLayer;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    tracing_subscriber::fmt()
+        .with_env_filter(
+            tracing_subscriber::EnvFilter::from_default_env()
+                .add_directive("synapse_money_api=debug".parse()?),
+        )
+        .init();
+
+    tracing::info!("Starting Synapse Money API...");
+
+    let app = Router::new()
+        .route("/health", get(health_check))
+        .layer(TraceLayer::new_for_http());
+
+    let addr = SocketAddr::from(([0, 0, 0, 0], 8080));
+    tracing::info!("🚀 Server listening on http://{}", addr);
+
+    let listener = tokio::net::TcpListener::bind(&addr).await?;
+    axum::serve(listener, app).await?;
+
+    Ok(())
+}
+
+async fn health_check() -> &'static str {
+    "OK"
+}
